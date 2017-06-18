5068c7888aab7d25cc1fe41ebd9e996dfb76fb14
Mercy <mercyblitz@gmail.com>
2017-06-18 23:46:55 +0800

    Add Lesson 3

---
+++ spring-boot/lesson-3/spring-boot-lession3/src/main/java/com/segmentfault/springbootlession3/SpringBootLession3Application.java
@@ -0,0 +1,12 @@
+package com.segmentfault.springbootlession3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLession3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLession3Application.class, args);
+	}
+}

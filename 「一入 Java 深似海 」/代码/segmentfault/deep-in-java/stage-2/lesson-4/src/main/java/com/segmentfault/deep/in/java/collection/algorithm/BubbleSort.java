c0b69297a6ea078e9e2143e3b9742bf0403944b0
heqian <469411502@qq.com>
2019-03-03 01:15:59 +0800

    If we add a while loop, we're going to move the high order to the right.

--- 「一入 Java 深似海 」/代码/segmentfault/deep-in-java/stage-2/lesson-4/src/main/java/com/segmentfault/deep/in/java/collection/algorithm/BubbleSort.java
+++ 「一入 Java 深似海 」/代码/segmentfault/deep-in-java/stage-2/lesson-4/src/main/java/com/segmentfault/deep/in/java/collection/algorithm/BubbleSort.java
@@ -16,22 +16,26 @@ public class BubbleSort<T extends Comparable<T>> implements Sort<T> {
         // Given array : [3,1,2,5,4]
         // for 1          |
         // for 2            |
-        for (int i = 0; i < size; i++) {
-            // 第 i 号元素
-            T t = values[i]; // 产生临时变量
-            for (int j = i + 1; j < size; j++) {
-                // 第 i 号元素与 i + 1 对比
-                if (t.compareTo(values[j]) == 1) { // 低位 > 高位
-                    // 交换元素 [i + 1] = [i]
-                    values[i] = values[j];
-                    values[j] = t;
-                    // [0] = 3 , [1] = 2
-                    // [1] = [1](2) + [0](3) = 5
-                    // [0] = [1](5) - [0](3) = 2
-                    // [1] = [1](5) - [0](2) = 3
-                    break;
+        int count = size - 1;
+        while ( count >= 0) {
+            for (int i = 0; i < size; i++) {
+                // 第 i 号元素
+                T t = values[i]; // 产生临时变量
+                for (int j = i + 1; j < size; j++) {
+                    // 第 i 号元素与 i + 1 对比
+                    if (t.compareTo(values[j]) == 1) { // 低位 > 高位
+                        // 交换元素 [i + 1] = [i]
+                        values[i] = values[j];
+                        values[j] = t;
+                        // [0] = 3 , [1] = 2
+                        // [1] = [1](2) + [0](3) = 5
+                        // [0] = [1](5) - [0](3) = 2
+                        // [1] = [1](5) - [0](2) = 3
+                        break;
+                    }
                 }
             }
+            count--;
         }
     }
 
